9db38828717a28d0b428f51422cb832d8082c006
TaeBbong <mok05289@korea.ac.kr>
2025-04-11 01:10:38 +0900

    wip: app overall configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/themes/dark_theme.dart';
 import 'core/themes/light_theme.dart';
+import 'presentation/pages/admin_page.dart';
 import 'presentation/pages/init_page.dart';
+import 'presentation/pages/main_page.dart';
+import 'presentation/pages/my_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,11 +18,17 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InitPage(),
+      home: MyPage(),
       debugShowCheckedModeBanner: false,
       theme: LightTheme.theme,
       darkTheme: DarkTheme.theme,
       themeMode: ThemeMode.system,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('ko', 'KR'), Locale('en', 'US')],
     );
   }
 }
