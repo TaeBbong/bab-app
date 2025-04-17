e789eee29b08f745e14059fc93b1a0d82acc43ce
TaeBbong <mok05289@korea.ac.kr>
2025-04-17 19:47:56 +0900

    chore: build_runner build

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -16,6 +16,12 @@ import 'package:bab/data/sources/eating_remote_data_source.dart' as _i768;
 import 'package:bab/data/sources/user_info_local_data_source.dart' as _i748;
 import 'package:bab/domain/repositories/eating_repository.dart' as _i67;
 import 'package:bab/domain/repositories/user_info_repository.dart' as _i507;
+import 'package:bab/domain/usecases/apply_eat_usecase.dart' as _i209;
+import 'package:bab/domain/usecases/cancel_eat_usecase.dart' as _i316;
+import 'package:bab/domain/usecases/get_user_info_usecase.dart' as _i315;
+import 'package:bab/domain/usecases/monthly_all_eat_usecase.dart' as _i990;
+import 'package:bab/domain/usecases/monthly_user_eat_usecase.dart' as _i221;
+import 'package:bab/domain/usecases/overall_eat_usecase.dart' as _i995;
 import 'package:bab/domain/usecases/register_user_usecase.dart' as _i476;
 import 'package:cloud_firestore/cloud_firestore.dart' as _i974;
 import 'package:get_it/get_it.dart' as _i174;
@@ -47,11 +53,29 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i476.RegisterUserUsecase>(
       () => _i476.RegisterUserUsecase(gh<_i507.UserInfoRepository>()),
     );
+    gh.factory<_i315.GetUserInfoUsecase>(
+      () => _i315.GetUserInfoUsecase(gh<_i507.UserInfoRepository>()),
+    );
     gh.lazySingleton<_i67.EatingRepository>(
       () => _i80.EatingRepositoryImpl(
         gh<_i768.EatingRemoteDataSource>(),
         gh<_i748.UserInfoLocalDataSource>(),
       ),
+    );
+    gh.factory<_i209.ApplyEatUsecase>(
+      () => _i209.ApplyEatUsecase(gh<_i67.EatingRepository>()),
+    );
+    gh.factory<_i316.CancelEatUsecase>(
+      () => _i316.CancelEatUsecase(gh<_i67.EatingRepository>()),
+    );
+    gh.factory<_i221.MonthlyUserEatUsecase>(
+      () => _i221.MonthlyUserEatUsecase(gh<_i67.EatingRepository>()),
+    );
+    gh.factory<_i990.MonthlyAllEatUsecase>(
+      () => _i990.MonthlyAllEatUsecase(gh<_i67.EatingRepository>()),
+    );
+    gh.factory<_i995.OverallEatUsecase>(
+      () => _i995.OverallEatUsecase(gh<_i67.EatingRepository>()),
     );
     return this;
   }
