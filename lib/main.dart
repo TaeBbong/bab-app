849d42a3d5d5f88a25a9df2f134a0e3da90681ca
TaeBbong <mok05289@korea.ac.kr>
2025-04-17 20:47:16 +0900

    chore: firebase initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
@@ -14,6 +15,7 @@ import 'presentation/pages/my_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await configureDependencies();
 
   final UserInfoLocalDataSource userInfoLocalDataSource =
