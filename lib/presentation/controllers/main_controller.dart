03fe3db7e12320a89e284c594352726c2b0fdbd1
TaeBbong <mok05289@korea.ac.kr>
2025-04-17 14:43:08 +0900

    feat: add Controller for MainPage

--- lib/presentation/controllers/main_controller.dart
+++ lib/presentation/controllers/main_controller.dart
@@ -1,24 +1,35 @@
 import 'package:get/get.dart';
 
 import '../../core/di/di.dart';
+import '../../core/utils/date_utils.dart';
 import '../../domain/entities/eating.dart';
+import '../../domain/entities/user_info.dart';
 import '../../domain/usecases/apply_eat_usecase.dart';
 import '../../domain/usecases/cancel_eat_usecase.dart';
 import '../../domain/usecases/daily_all_eat_usecase.dart';
 import '../../domain/usecases/daily_user_eat_usecase.dart';
 import '../../domain/usecases/monthly_all_eat_usecase.dart';
+import '../../domain/usecases/monthly_user_eat_usecase.dart';
+import '../../domain/usecases/get_user_info_usecase.dart';
 
 class MainController extends GetxController {
   final MonthlyAllEatUsecase monthlyAllEatUsecase =
       getIt<MonthlyAllEatUsecase>();
+  final MonthlyUserEatUsecase monthlyUserEatUsecase =
+      getIt<MonthlyUserEatUsecase>();
   final DailyAllEatUsecase dailyAllEatUsecase = getIt<DailyAllEatUsecase>();
   final DailyUserEatUsecase dailyUserEatUsecase = getIt<DailyUserEatUsecase>();
   final ApplyEatUsecase applyEatUsecase = getIt<ApplyEatUsecase>();
   final CancelEatUsecase cancelEatUsecase = getIt<CancelEatUsecase>();
+  final GetUserInfoUsecase getUserInfoUsecase = getIt<GetUserInfoUsecase>();
 
   Rx<DateTime> focusedDay = DateTime.now().obs;
   Rx<DateTime> selectedDay = DateTime.now().obs;
-  RxBool isApplied = false.obs;
+
+  RxMap<DateTime, bool> monthlyUserEatingMap = <DateTime, bool>{}.obs;
+  RxMap<DateTime, List<Eating>> monthlyAllEatingMap =
+      <DateTime, List<Eating>>{}.obs;
+  Rx<UserInfo> userInfo = UserInfo(username: '', group: '').obs;
 
   void onDaySelected(DateTime selected, DateTime focused) {
     focusedDay.value = selected;
@@ -29,38 +40,99 @@ class MainController extends GetxController {
     focusedDay.value = focused;
   }
 
-  RxMap<DateTime, List<Eating>> dailyEatingMap = <DateTime, List<Eating>>{}.obs;
+  /// Handler which is onPressed MainPage's apply/cancel button.
+  ///
+  ///
+  Future<void> applyEating() async {
+    final DateTime applyDate = MyDateUtils.onlyDates(selectedDay.value);
 
-  Future<void> applyEating({required DateTime eatDate}) async {
-    await applyEatUsecase.execute(eatDate: eatDate);
+    final Eating applyResult = await applyEatUsecase.execute(
+      eatDate: applyDate,
+    );
+
+    monthlyUserEatingMap.assignAll({...monthlyUserEatingMap, applyDate: true});
+
+    final currentList = monthlyAllEatingMap[applyDate];
+    if (currentList != null) {
+      currentList.add(applyResult);
+      monthlyAllEatingMap[applyDate] = List.from(currentList);
+    } else {
+      monthlyAllEatingMap[applyDate] = [applyResult];
+    }
+
     Get.snackbar('신청 완료', '식사 신청이 완료되었습니다.');
   }
 
-  Future<void> cancelEating({required Eating eating}) async {
-    await cancelEatUsecase.execute(docId: eating.id);
+  /// Handler which is onPressed MainPage's apply/cancel button.
+  ///
+  /// Find docId / eating object from where?
+  Future<void> cancelEating() async {
+    final DateTime cancelDate = MyDateUtils.onlyDates(selectedDay.value);
+    final UserInfo? userInfo = await getUserInfoUsecase.execute();
+    String docId = '';
+
+    if (userInfo == null) {
+      Get.snackbar('Error occured!', 'Invalid request from unknown user');
+      return;
+    }
+
+    if (monthlyAllEatingMap[cancelDate] == null) {
+      Get.snackbar('Error occured!', 'Invalid request from empty list in date');
+      return;
+    }
+
+    for (Eating eating in monthlyAllEatingMap[cancelDate]!) {
+      if (eating.username == userInfo.username) {
+        docId = eating.id;
+        break;
+      }
+    }
+
+    if (docId == '') {
+      Get.snackbar('Error occured!', 'Invalid request docid not found');
+      return;
+    }
+
+    await cancelEatUsecase.execute(docId: docId);
+
+    monthlyUserEatingMap.assignAll({
+      ...monthlyUserEatingMap,
+      cancelDate: false,
+    });
+
+    final List<Eating>? list = monthlyAllEatingMap[cancelDate];
+    if (list != null) {
+      list.removeWhere((e) => e.id == docId);
+      monthlyAllEatingMap[cancelDate] = List.from(list);
+    }
+
     Get.snackbar('취소 완료', '식사 신청이 취소되었습니다.');
   }
 
-  Future<void> getMonthlyAllEatings({required DateTime month}) async {
-    final List<Eating> eatings = await monthlyAllEatUsecase.execute(
-      date: month,
-    );
+  /// Load all monthly eatings when MainPage renders.
+  Future<void> getMonthlyAllEatings() async {
+    final Map<DateTime, List<Eating>> monthlyAllEatingStatus =
+        await monthlyAllEatUsecase.execute(focusedDay: focusedDay.value);
 
-    final Map<DateTime, List<Eating>> grouped = {};
+    monthlyAllEatingMap.assignAll(monthlyAllEatingStatus);
+  }
 
-    for (final eating in eatings) {
-      final date = DateTime(
-        eating.eatDate.year,
-        eating.eatDate.month,
-        eating.eatDate.day,
-      );
+  /// Load users' monthly eatings when MainPage renders, to show emoji on calendar.
+  ///
+  /// Runs for focusedDay
+  Future<void> getMonthlyUserEatings() async {
+    userInfo.value = await getUserInfoUsecase.execute();
 
-      if (!grouped.containsKey(date)) {
-        grouped[date] = [];
-      }
-      grouped[date]!.add(eating);
-    }
+    final Map<DateTime, bool> userEatingStatus = await monthlyUserEatUsecase
+        .execute(focusedDay: focusedDay.value);
 
-    dailyEatingMap.assignAll(grouped);
+    monthlyUserEatingMap.assignAll(userEatingStatus);
+  }
+
+  List<String> getDailyAppliedUsers() {
+    final List<Eating> dailyEatings = monthlyAllEatingMap[focusedDay.value]!;
+    final List<String> dailyAppliedUsers =
+        dailyEatings.map((e) => e.username).toList();
+    return dailyAppliedUsers;
   }
 }
--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -7,12 +7,31 @@ import '../../core/themes/box_decoration_theme.dart';
 import '../../core/themes/palette.dart';
 import '../controllers/main_controller.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final MainController controller = Get.put(MainController());
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getMonthlyAllEatings();
+    controller.getMonthlyUserEatings();
+  }
+
+  @override
+  void dispose() {
+    Get.delete<MainController>();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final ThemeData appTheme = Theme.of(context);
     final TextTheme textTheme = appTheme.textTheme;
-    final MainController controller = Get.put(MainController());
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
@@ -22,131 +41,156 @@ class MainPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            children: [
-              Text('⏰ 신청/취소 마감은 당일 08:30입니다. ⏰'),
-              Text('⏰ 당일 08:30까지 입금하지 않으면 자동 취소됩니다. ⏰'),
-              TableCalendar(
-                locale: 'ko_KR',
-                firstDay: DateTime(2000, 1, 1),
-                lastDay: DateTime(2100, 12, 31),
-                focusedDay: DateTime.now(),
-                // focusedDay: _controller.focusedDay.value,
-                // selectedDayPredicate:
-                //     (day) => isSameDay(_controller.selectedDay.value, day),
-                // onDaySelected: (selectedDay, focusedDay) {
-                //   _controller.onDaySelected(selectedDay, focusedDay);
-                // },
-                // onPageChanged: (focusedDay) {
-                //   _controller.onPageChanged(focusedDay);
-                //   _controller.getSchedulesForMonth(focusedDay);
-                // },
-                calendarFormat: CalendarFormat.month,
-                availableCalendarFormats: const {CalendarFormat.month: 'Month'},
-                daysOfWeekHeight: 40,
-                headerStyle: const HeaderStyle(
-                  formatButtonVisible: false,
-                  titleCentered: true,
-                ),
-                calendarStyle: CalendarStyle(
-                  outsideDaysVisible: false,
-                  todayDecoration: BoxDecoration(
-                    color: Palette.beige100,
-                    shape: BoxShape.circle,
-                  ),
-                  selectedDecoration: BoxDecoration(
-                    color: Palette.beige,
-                    shape: BoxShape.circle,
-                  ),
-                  defaultTextStyle: const TextStyle(fontSize: 16),
-                  selectedTextStyle: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
-                  ),
-                  todayTextStyle: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
-                  ),
-                ),
-
-                calendarBuilders: CalendarBuilders(
-                  dowBuilder: (context, day) {
-                    final text = MyDateUtils.weekdayLabel(day.weekday);
-                    Color? color;
-                    if (day.weekday + 1 == DateTime.sunday) {
-                      color = Colors.red;
-                    } else if (day.weekday + 1 == DateTime.saturday) {
-                      color = Colors.blue;
-                    }
-                    return Center(
-                      child: Text(
-                        text,
-                        style: TextStyle(
-                          color: color,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    );
+          child: Obx(() {
+            final Map<DateTime, bool> monthlyUserEatingMap =
+                controller.monthlyUserEatingMap;
+            final List<String> appliedUsersOnFocusedDay =
+                controller.getDailyAppliedUsers();
+            return Column(
+              children: [
+                Text('⏰ 신청/취소 마감은 당일 08:30입니다. ⏰'),
+                Text('⏰ 당일 08:30까지 입금하지 않으면 자동 취소됩니다. ⏰'),
+                TableCalendar(
+                  locale: 'ko_KR',
+                  firstDay: DateTime(2000, 1, 1),
+                  lastDay: DateTime(2100, 12, 31),
+                  focusedDay: controller.focusedDay.value,
+                  selectedDayPredicate:
+                      (day) => isSameDay(controller.selectedDay.value, day),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    controller.onDaySelected(selectedDay, focusedDay);
                   },
-                  defaultBuilder: (context, date, events) {
-                    final hasEvent = true;
-                    // final hasEvent = events[DateUtils.dateOnly(day)]?.isNotEmpty ?? false;
-                    return Padding(
-                      padding: const EdgeInsets.all(4),
-                      child: Stack(
-                        children: [
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              '${date.day}',
-                              style: const TextStyle(fontSize: 14),
-                            ),
-                          ),
-                          if (hasEvent)
-                            const Align(
-                              alignment: Alignment.bottomRight,
-                              child: Text('🍱', style: TextStyle(fontSize: 16)),
-                            ),
-                        ],
-                      ),
-                    );
+                  onPageChanged: (focusedDay) {
+                    controller.onPageChanged(focusedDay);
+                    controller.getMonthlyAllEatings();
+                    controller.getMonthlyUserEatings();
                   },
-                ),
-              ),
-              const SizedBox(height: 24),
-              Container(
-                padding: const EdgeInsets.all(16),
-                // margin: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: appTheme.extension<BoxDecorationTheme>()!.card,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('오늘의 신청자', style: textTheme.titleMedium),
-                    const SizedBox(height: 4),
-                    Text(
-                      '권태형 김철수 박지철 윤철수 김철수 정현철 김지철 양철철 민철철 강현철',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: textTheme.bodySmall,
+                  calendarFormat: CalendarFormat.month,
+                  availableCalendarFormats: const {
+                    CalendarFormat.month: 'Month',
+                  },
+                  daysOfWeekHeight: 40,
+                  headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                  calendarStyle: CalendarStyle(
+                    outsideDaysVisible: false,
+                    todayDecoration: BoxDecoration(
+                      color: Palette.beige100,
+                      shape: BoxShape.circle,
                     ),
-                    const SizedBox(height: 16),
-                    Text('오늘 내 신청 상태', style: textTheme.titleMedium),
-                    SizedBox(height: 4),
-                    Text('미신청', style: textTheme.labelMedium),
-                  ],
+                    selectedDecoration: BoxDecoration(
+                      color: Palette.beige,
+                      shape: BoxShape.circle,
+                    ),
+                    defaultTextStyle: const TextStyle(fontSize: 16),
+                    selectedTextStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                    todayTextStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+
+                  calendarBuilders: CalendarBuilders(
+                    dowBuilder: (context, day) {
+                      final text = MyDateUtils.weekdayLabel(day.weekday);
+                      Color? color;
+                      if (day.weekday + 1 == DateTime.sunday) {
+                        color = Colors.red;
+                      } else if (day.weekday + 1 == DateTime.saturday) {
+                        color = Colors.blue;
+                      }
+                      return Center(
+                        child: Text(
+                          text,
+                          style: TextStyle(
+                            color: color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      );
+                    },
+                    defaultBuilder: (context, date, events) {
+                      final bool hasAppliedOnDate =
+                          monthlyUserEatingMap[MyDateUtils.onlyDates(date)]!;
+                      return Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                '${date.day}',
+                                style: const TextStyle(fontSize: 14),
+                              ),
+                            ),
+                            if (hasAppliedOnDate)
+                              const Align(
+                                alignment: Alignment.bottomRight,
+                                child: Text(
+                                  '🍱',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              const Spacer(),
-              SizedBox(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('식사 신청하기'),
+                const SizedBox(height: 24),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  // margin: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: appTheme.extension<BoxDecorationTheme>()!.card,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('오늘의 신청자', style: textTheme.titleMedium),
+                      const SizedBox(height: 4),
+                      Text(
+                        appliedUsersOnFocusedDay.join(', '),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: textTheme.bodySmall,
+                      ),
+                      const SizedBox(height: 16),
+                      Text('오늘 내 신청 상태', style: textTheme.titleMedium),
+                      SizedBox(height: 4),
+                      monthlyUserEatingMap[MyDateUtils.onlyDates(
+                            controller.focusedDay.value,
+                          )]!
+                          ? Text('신청', style: textTheme.labelMedium)
+                          : Text('미신청', style: textTheme.labelMedium),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
+                const Spacer(),
+                SizedBox(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed:
+                        monthlyUserEatingMap[MyDateUtils.onlyDates(
+                              controller.focusedDay.value,
+                            )]!
+                            ? controller.cancelEating
+                            : controller.applyEating,
+                    child:
+                        monthlyUserEatingMap[MyDateUtils.onlyDates(
+                              controller.focusedDay.value,
+                            )]!
+                            ? const Text('식사 신청하기')
+                            : const Text('식사 취소하기'),
+                  ),
+                ),
+              ],
+            );
+          }),
         ),
       ),
     );
